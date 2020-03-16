395a759738768f74713cc9c1c6a397fa39c3a238
Jordan Whitten <jordanwa1947@gmail.com>
2020-03-16 17:14:10 -0600

    Import NewsContainer component and nest it within the App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,18 +2,20 @@ import React, { Component } from 'react';
 import local from '../../data/local';
 import './App.css';
 
+import NewsContainer from '../NewsContainer/NewsContainer';
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      localArticles: local
     }
   }
 
   render () {
     return (
       <div className="app">
-        YOUR CODE GOES HERE!
+        <NewsContainer />
       </div>
     );
   }
