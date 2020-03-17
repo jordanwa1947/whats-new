1bea8e60b14ab6ffd48bcce8226a37a0247de33c
Jordan Whitten <jordanwa1947@gmail.com>
2020-03-17 16:37:37 -0600

    Import article data, add it to state and add display article method

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
+
 import local from '../../data/local';
+import health from '../../data/health';
+import entertainment from '../../data/entertainment';
+import science from '../../data/science';
+import technology from '../../data/technology';
+
 import './App.css';
 
 import NewsContainer from '../NewsContainer/NewsContainer';
@@ -9,15 +15,26 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      localArticles: local
+      selectedArticles: local,
+      local: local,
+      health: health,
+      entertainment: entertainment,
+      technology: technology,
+      science: science
     }
+  }
+
+  displayArticles = (category) => {
+    this.setState({
+      selectedArticles: this.state[category]
+    })
   }
 
   render () {
     return (
       <div className="app">
-        <Menu />
-        <NewsContainer localArticles={this.state.localArticles} />
+        <Menu displayArticles={this.displayArticles}/>
+        <NewsContainer articles={this.state.selectedArticles} />
       </div>
     );
   }
