27f78565a89dd5cbfa965a2a28a919315e0d45d3
Jordan Whitten <jordanwa1947@gmail.com>
2020-03-16 20:08:27 -0600

    Creates an array of NewArticle components and renders them in the NewsContainer component

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './NewsContainer.css'
+import NewsArticle from '../NewsArticle/NewsArticle'
 
 function NewsContainer(props) {
+   const articleComponents = props.localArticles.map(article => {
+     return <NewsArticle key={article.id} article={article} />
+   })
   return (
     <section className="new-container">
-      <p>Test</p>
+      {articleComponents}
     </section>
   )
 }
