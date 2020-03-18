b58b939394f6f2e3d4dc732b9dec799ec28d6cd2
Jordan Whitten <jordanwa1947@gmail.com>
2020-03-18 09:28:36 -0600

    Adds the searchArticles method to state in the SearchForm component and a handleSearch callback function that, when the search button is clicked, invokes searchArticles

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './SearchForm.css';
 
 class SearchForm extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
     this.state = {
-      query: ''
+      query: '',
+      searchArticles: props.searchArticles
     }
   }
 
@@ -15,11 +16,15 @@ class SearchForm extends React.Component {
     })
   }
 
+  handleSearch = () => {
+    this.state.searchArticles(this.state.query)
+  }
+
   render() {
     return (
       <form>
         <input type="text" value={this.state.query} onChange={this.handleChange} />
-        <button>Search</button>
+        <button type="button" onClick={this.handleSearch}>Search</button>
       </form>
     )
   }
