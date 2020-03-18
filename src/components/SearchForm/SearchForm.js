f895deff495cbc7924b845e007e1384fea6bccfa
Jordan Whitten <jordanwa1947@gmail.com>
2020-03-17 18:57:34 -0600

    Add SearchForm component with handle change method to update the search field as a user types

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import './SearchForm.css';
 
-// SEARCHFORM COMPONENT CODE GOES HERE
+class SearchForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      query: ''
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      query: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <form>
+        <input type="text" value={this.state.query} onChange={this.handleChange} />
+        <button>Search</button>
+      </form>
+    )
+  }
+}
 
 export default SearchForm;
