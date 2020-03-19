bdf01d844a6057fa6c034dd7d39a5704d922f062
Jordan Whitten <jordanwa1947@gmail.com>
2020-03-19 13:10:23 -0600

    Adds basic test for rendered text for news article component

---
+++ src/components/NewsArticle/NewsArticle.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { render } from '@testing-library/react'; 
+import NewsArticle from './NewsArticle.js';
+ import '@testing-library/jest-dom'
+
+describe('NewsArticle', () => {
+  it('It should render the text that we expect', () => {
+    const { getByText } = render(<NewsArticle
+      article={{
+        id: 1,
+        headline: 'The Who postpones Denver Concert at the Pepsi Center',
+        img: 'https://theknow.denverpost.com/wp-content/uploads/2016/03/CDXXTHEWHODSC_4082x.jpg',
+        description: 'Classic-rock band The Who has postponed its Denver concert, set for Sunday, Sept. 29, after an illness robbed singer Roger Daltrey of his voice, the band said on Twitter.',
+        url: 'https://theknow.denverpost.com/2019/09/27/the-who-postpones-denver-concert/225182/'
+      }}
+    />);
+
+    expect(getByText("The Who postpones Denver Concert at the Pepsi Center")).toBeInTheDocument();
+    expect(getByText("Classic-rock band The Who has postponed its Denver concert, set for Sunday, Sept. 29, after an illness robbed singer Roger Daltrey of his voice, the band said on Twitter.")).toBeInTheDocument();
+  })
+})
