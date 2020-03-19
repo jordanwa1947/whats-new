70233e81b98823cbe4dcb5d16f07019860cd2eaa
Jordan Whitten <jordanwa1947@gmail.com>
2020-03-19 14:08:49 -0600

    Adds App component integration test for search functionality

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,8 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import  { render, fireEvent } from '@testing-library/react'; 
+ import '@testing-library/jest-dom'
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('can search for articles', () => {
+    const { getByText, getByPlaceholderText } = render(<App />)
+
+    fireEvent.change(getByPlaceholderText(/Search/i), { target: { value: 'who' } })
+    fireEvent.click(getByText("Search"));
+
+    expect(getByText("The Who postpones Denver Concert at the Pepsi Center")).toBeInTheDocument();
+    expect(getByText("Classic-rock band The Who has postponed its Denver concert, set for Sunday, Sept. 29, after an illness robbed singer Roger Daltrey of his voice, the band said on Twitter.")).toBeInTheDocument();
+  })
+
+})
